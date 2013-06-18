30117c5f81e21093a8e945e4b795fa14cfb6873a
Jithu Maliakal <jithu.maliakal@gmail.com>
2013-06-17 23:18:30 -0400

    rails new

---
+++ rodeo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rodeo::Application.config.secret_token = '06337aeb9083455aaab3c00aaa097c129b18952b81fd9775d70e84f90225b73fd1b1b36221e69c5d609174724d1f6762526a78d7731a1b767705c0e0715e7a24'
